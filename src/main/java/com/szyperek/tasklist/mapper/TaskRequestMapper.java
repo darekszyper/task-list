29061198173634970fa013464d85f832697e0ded
darekszyper <114878453+darekszyper@users.noreply.github.com>
2023-07-22 20:08:21 +0200

    hotfix

--- src/main/java/com/szyperek/tasklist/mapper/TaskRequestMapper.java
+++ src/main/java/com/szyperek/tasklist/mapper/TaskRequestMapper.java
@@ -2,15 +2,14 @@ package com.szyperek.tasklist.mapper;
 
 import com.szyperek.tasklist.dto.request.TaskRequest;
 import com.szyperek.tasklist.entity.TaskEntity;
-import org.mapstruct.AfterMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
+import org.mapstruct.*;
 
 import java.time.LocalDateTime;
 
 @Mapper(componentModel = "spring")
 public abstract class TaskRequestMapper {
 
+    @BeanMapping(builder = @Builder(disableBuilder = true))
     public abstract TaskEntity mapTaskRequestToTaskEntity(TaskRequest taskRequest);
 
     @AfterMapping
